20371fc6fc6ec5bd7e1005f5436c510d65f873de
madhug90 <46104535+madhug90@users.noreply.github.com>
2024-12-22 22:39:38 +0000

    Update cse.ps1

--- cse.ps1
+++ cse.ps1
@@ -1,1 +1,20 @@
-get-date > C:\temp.txt
+$ErrorActionPreference="Stop";
+If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator")){ throw "Run command in an administrator PowerShell prompt"};
+If($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0"))){ throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell." };
+If(-NOT (Test-Path $env:SystemDrive\'azagent')){mkdir $env:SystemDrive\'azagent'}; 
+cd $env:SystemDrive\'azagent'; 
+for($i=1; $i -lt 100; $i++){
+	$destFolder="A"+$i.ToString();if(-NOT (Test-Path ($destFolder))){mkdir $destFolder;cd $destFolder;break;}
+};
+$agentZip="$PWD\agent.zip";
+$DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;
+$securityProtocol=@();
+$securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;
+$securityProtocol+=[Net.SecurityProtocolType]::Tls12;[Net.ServicePointManager]::SecurityProtocol=$securityProtocol;
+$WebClient=New-Object Net.WebClient; 
+$Uri='https://vstsagentpackage.azureedge.net/agent/4.248.0/vsts-agent-win-x64-4.248.0.zip';
+if($DefaultProxy -and (-not $DefaultProxy.IsBypassed($Uri))){$WebClient.Proxy= New-Object Net.WebProxy($DefaultProxy.GetProxy($Uri).OriginalString, $True);}; 
+$WebClient.DownloadFile($Uri, $agentZip);
+Add-Type -AssemblyName System.IO.Compression.FileSystem;[System.IO.Compression.ZipFile]::ExtractToDirectory( $agentZip, "$PWD");
+.\config.cmd --environment --environmentname "KK_Linux_Dev" --agent $env:COMPUTERNAME --runasservice --work '_work' --url 'https://dev.azure.com/udhamg/' --projectname 'ADO_Practice' --auth PAT --token AFlYOt2M5ShApCekN0kAbK8EoQilodDBx8SgvLoiRVatIMCdVuvLJQQJ99ALACAAAAAAAAAAAAASAZDOACnI;
+Remove-Item $agentZip;
